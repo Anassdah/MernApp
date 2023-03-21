11712f901977705633fa4816fd75c9880fbad02e
AnassDah <anassdah5@gmail.com>
2023-03-21 16:31:23 +0100

    added some tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor} from '@testing-library/react';
 import App from './App';
+describe('test diplay',()=> {
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test('renders the landing page', () => {
+    render(<App />);
+  });
+  test('renders Hello world', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Hello world/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+})
+
+
+describe('API endpoint', () => {
+  it('should return the expected data', async () => {
+    // Make a request to your API endpoint
+    const response = await fetch('http://localhost:4000/api/items');
+
+    // Wait for the response to be available
+    await waitFor(() => {
+      expect(response.status).toBe(200); // Check the status code
+    });
+
+
+  });
 });
+
